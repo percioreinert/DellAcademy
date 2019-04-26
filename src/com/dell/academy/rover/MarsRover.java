41407e3be97050866ad8e8eb3a8f6bff27c0e396
percioreinert <percioreinert@gmail.com>
2019-04-26 16:51:38 -0300

    Updates

--- src/com/dell/academy/rover/MarsRover.java
+++ src/com/dell/academy/rover/MarsRover.java
@@ -3,7 +3,28 @@ package com.dell.academy.rover;
 public class MarsRover {
 
     // == fields ==
-    private Position position;
+    private int x;
+    private int y;
     private Movement movement;
+    // TODO: finish the implementation.
 
+    // == constructors ==
+    public MarsRover(int x, int y) {
+        this.x = x;
+        this.y = y;
+        movement = Movement.M;
+    }
+
+    // == public methods ==
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public Movement getMovement() {
+        return this.movement;
+    }
 }
